43c678426d25c380df5eb8ec8543456829a05c44
Denakosg945 <denakosg@gmail.com>
2026-08-19 18:33:52 +0300

    Server side almost complete - need encryption

--- server.c
+++ server.c
@@ -12,24 +12,99 @@
 
 #define MAX_CONNECTED_USERS 10
 #define POLL_WAITING_TIME 1000  // in ms 
+#define NAME_SIZE 256
+#define MAXIMUM_MSG_SIZE 2048 //2048 chars per message 
 
 int thread_counter = 0;
 //Create a pthread mutex so we can have exclusive control over the thread_counter
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
+//Create a pthread mutex to be used with the condition variable for reading msg buffer 
+pthread_mutex_t cond_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond_variable = PTHREAD_COND_INITIALIZER;
+int condition = true;
+int fd_pool[MAX_CONNECTED_USERS];
+
+void sendMessage(int fd,char *name,char* str){
+        send(fd,"\r\n",strlen("\r\n"),0);
+        send(fd,name,strlen(name),0);
+        send(fd,">",strlen(">"),0);
+        send(fd,str,strlen(str),0);
+        //Add error checking to send syscalls
+}
 
 void * thread_worker(void *connfd){
 
   
   int fd = (int)(intptr_t)connfd;
 
-  printf("Connection established with = %d",fd);
+  printf("Connection established with = %d\nWaiting for name...\n",fd);
+  //+1 for null termination
+  char name[NAME_SIZE+1];
+  name[NAME_SIZE] = '\0';
+
+
+  int bytes = recv(fd,name,NAME_SIZE,0);
+  
+  //if the server didn't receive a name send a message a new name 
+  while(bytes <= 2){
+    send(fd,"Please provide a correct username\n",strlen("Please provide a correct username\n"),0);
+    //Clear previous name 
+    memset(name,0,NAME_SIZE+1);
+    bytes = recv(fd,name,NAME_SIZE,0);
+  }
+
+
+  //Format the string to remove \n character 
+  char *delimiter = "\r";
+  char *token = strtok(name,delimiter);
+ 
+  //Ensure null termination on the name 
+  *(token+strlen(name)) = '\0';
+  
+  printf("Name received = %s(%d bytes)\n",name,bytes);
+  
+  printf("Beginning chat...");
+
+  char temporary_msg[MAXIMUM_MSG_SIZE+1];
+  memset(temporary_msg,0,MAXIMUM_MSG_SIZE);
   
   
+
+  int msgBytes = 0;
+  while(1){
+    send(fd,name,strlen(name),0);
+    send(fd,">",strlen(">"),0);
+
+    msgBytes = recv(fd,temporary_msg,MAXIMUM_MSG_SIZE,0);
+    if(msgBytes <= 0){
+      continue;
+    }
+
+    printf("Message received = %s (%d bytes)",temporary_msg,msgBytes);
+   
+    
+
+    
+    for(int i=0; i<MAX_CONNECTED_USERS; i++){
+      if(fd_pool[i] != -1){
+        sendMessage(fd_pool[i],name,temporary_msg);
+      }
+    }  
+    
+    memset(temporary_msg,0,MAXIMUM_MSG_SIZE+1);
+  }
+
+  for(int i=0; i<MAX_CONNECTED_USERS; i++){
+    if(fd == fd_pool[i]){
+      fd_pool[i] = -1;
+      break;
+    }
+  }
   close(fd);
 
   printf("Closed connection!\n");
-  
+
   //Critical region - can be edited by multiple threads/main at the same time causing race condition 
   pthread_mutex_lock(&mutex);
   thread_counter--;
@@ -119,21 +194,29 @@ int main(int argc,char **argv){
   fds.fd = fd;
   fds.events = POLLIN;
   //Make variable to store and transfer connection file descriptor to thread_worker 
-  int connfd; 
+  int current_idx; 
   int thr_ret;
+
+  for(int i=0; i<MAX_CONNECTED_USERS; i++){
+    fd_pool[i] = -1;
+  }
 
   while(1){
     
     if(poll(&fds,1,POLL_WAITING_TIME) == POLLIN){
       printf("Data incoming...\n");
-      connfd = accept(fd,NULL,NULL);
-      if(connfd == -1){
-        perror("Failed to accept connection...\n");
-        continue;
+      for(int i=0; i<MAX_CONNECTED_USERS; i++){
+        //Search for a freed slot
+        if(fd_pool[i] == -1){
+          fd_pool[i] = accept(fd,NULL,NULL);
+          current_idx = i;
+          break;
+        }
       }
       
+      
 
-      thr_ret = pthread_create(&pid[thread_counter],&thread_attr,thread_worker,(void*)(intptr_t)connfd);
+      thr_ret = pthread_create(&pid[thread_counter],&thread_attr,thread_worker,(void*)(intptr_t)fd_pool[current_idx]);
       if(thr_ret != 0){
         fprintf(stderr,"pthread_create: %s (%d)",strerror(thr_ret),thr_ret);
         continue;
