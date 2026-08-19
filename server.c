68a4cbe13d713aa10ed8516437c90139f07e575f
Denakosg945 <denakosg@gmail.com>
2026-08-19 15:46:00 +0300

    Added threads for each client - no chat yet

--- server.c
+++ server.c
@@ -5,18 +5,65 @@
 #include <errno.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <poll.h>
+#include <string.h>
+#include <stdint.h>
 
 
 #define MAX_CONNECTED_USERS 10
+#define POLL_WAITING_TIME 1000  // in ms 
+
+int thread_counter = 0;
+//Create a pthread mutex so we can have exclusive control over the thread_counter
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
+void * thread_worker(void *connfd){
+
+  
+  int fd = (int)(intptr_t)connfd;
+
+  printf("Connection established with = %d",fd);
+  
+  
+  close(fd);
+
+  printf("Closed connection!\n");
+  
+  //Critical region - can be edited by multiple threads/main at the same time causing race condition 
+  pthread_mutex_lock(&mutex);
+  thread_counter--;
+  pthread_mutex_unlock(&mutex);
+
+  return NULL; 
+}
 
 int main(int argc,char **argv){
 
   pthread_t pid[MAX_CONNECTED_USERS];
-
   //Create a return value variable to store return values to check validity
   int ret;
+  
+  //Set pthread attributes
+  pthread_attr_t thread_attr;
+  
+  
+  ret = pthread_attr_init(&thread_attr);
+    
+  if(ret != 0){
+    fprintf(stderr,"pthread_attr_init: %s (%d)\n",strerror(ret),ret);
+    exit(100);
+  }
+
+  ret = pthread_attr_setdetachstate(&thread_attr,PTHREAD_CREATE_DETACHED);
+
+  if(ret != 0){
+    fprintf(stderr,"pthread_attr_setdetachstate: %s (%d)\n",strerror(ret),ret);
+    exit(101);
+  }
+  
+  
+
   ret = socket(AF_INET,SOCK_STREAM,0);
 
   if(ret == -1){
@@ -63,8 +110,48 @@ int main(int argc,char **argv){
     exit(4);
   }
 
-  
+  printf("Listening for incoming connections...\n");
 
+  //Create a poll struct so we can monitor when a connection arrives
+  //
+
+  struct pollfd fds;
+  fds.fd = fd;
+  fds.events = POLLIN;
+  //Make variable to store and transfer connection file descriptor to thread_worker 
+  int connfd; 
+  int thr_ret;
+
+  while(1){
+    
+    if(poll(&fds,1,POLL_WAITING_TIME) == POLLIN){
+      printf("Data incoming...\n");
+      connfd = accept(fd,NULL,NULL);
+      if(connfd == -1){
+        perror("Failed to accept connection...\n");
+        continue;
+      }
+      
+
+      thr_ret = pthread_create(&pid[thread_counter],&thread_attr,thread_worker,(void*)(intptr_t)connfd);
+      if(thr_ret != 0){
+        fprintf(stderr,"pthread_create: %s (%d)",strerror(thr_ret),thr_ret);
+        continue;
+      }  
+      
+      pthread_mutex_lock(&mutex);
+      thread_counter++;
+      pthread_mutex_unlock(&mutex);
+      printf("Thread created\n");
+
+
+    }
+    
+
+
+  }  
+
+  pthread_mutex_destroy(&mutex);
 
   return 0;
 
